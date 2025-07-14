af97a9a7c11c5e9d410d85b80043971914151f05
Saúl Díaz <sauldiazdiaz210@gmail.com>
2025-07-14 10:55:54 +0200

    Update plugin.php

--- plugin.php
+++ plugin.php
@@ -3,8 +3,8 @@ $id = "silr";
 
 $version = "0.0.1";
 
-$ilias_min_version = "9.0";
-$ilias_max_version = "9.99";
+$ilias_min_version = "7.0";
+$ilias_max_version = "7.99";
 
 $responsible = "Jesus Copado";
 $responsible_mail = "jcopado@surlabs.com";
